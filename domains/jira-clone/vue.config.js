14751b264b6e79c0b88628d8dbb9d2627bf420d0
Vladimir Gerasimchuk <var.alo@mail.ru>
2023-02-26 02:08:25 +0300

    add publicPath auto value

--- domains/jira-clone/vue.config.js
+++ domains/jira-clone/vue.config.js
@@ -7,6 +7,9 @@ const remoteWysiwyg = isProd ? 'https://vue-jira-wysiwyg.onrender.com/remoteEntr
 const remoteUI = isProd ? 'https://vue-jira-ui.onrender.com/remoteEntry.js' : 'http://localhost:3001/remoteEntry.js';
 
 module.exports = defineConfig({
+  output: {
+    publicPath: 'auto',
+  },
   css: {
     loaderOptions: {
       sass: {
@@ -34,7 +37,7 @@ module.exports = defineConfig({
             singleton: true,
           },
         },
-      })
+      }),
     ],
   },
 });
